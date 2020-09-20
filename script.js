74fc1f74b3452a903bbba29571fc85589a7d9f78
Ghoste <hypxwghoste@gmail.com>
2020-09-20 16:52:06 -0300

    Formatação

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function carregar() {
-    function hours () { //mostra a hora em tempo real
-        var hours = document.getElementById ('msg')
-        var date1 = new Date ()
+    function hours() { //mostra a hora em tempo real
+        var hours = document.getElementById('msg')
+        var date1 = new Date()
         var gthours = date1.getHours()
         var gtminutes = date1.getMinutes()
         var gtseconds = date1.getSeconds()
-    
-            if (gthours < 10) {
-                gthours = `0${gthours}`
-            }
-            if (gtminutes < 10) {
-                gtminutes = `0${gtminutes}`
-            }
-    
-            if (gtseconds < 10) {
-                gtseconds =`0${gtseconds}`
-            }         
+
+        if (gthours < 10) {
+            gthours = `0${gthours}`
+        }
+        if (gtminutes < 10) {
+            gtminutes = `0${gtminutes}`
+        }
+
+        if (gtseconds < 10) {
+            gtseconds = `0${gtseconds}`
+        }
         hours.textContent = `Agora são ${gthours} horas,${gtminutes} minutos e ${gtseconds} segundos`
     }
-        var tictac = setInterval (hours, 1000)
+    var tictac = setInterval(hours, 1000)
     hours()
     var data = new Date //mostra mensagem conforme a hora
     var hora = data.getHours()
